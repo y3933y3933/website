f0dd993233277299b159f5a778338d42f1e1337a
Joanne Lin <y3933y3933@gmail.com>
2026-05-17 23:28:47 +0800

    feat: mdx

---
+++ src/content.config.js
@@ -0,0 +1,17 @@
+import { defineCollection } from 'astro:content';
+import { glob, file } from 'astro/loaders';
+import { z } from 'astro/zod';
+
+
+const blog = defineCollection({
+    loader: glob({base :'./src/blog', pattern:'**/*.{md,mdx}'}),
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        pubDate: z.coerce.date(),
+        tags: z.array(z.string()).default([]),
+    })
+})
+
+
+export const collections = {blog}

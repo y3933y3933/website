53651c155d22bd5509a052afba2b4e02d579b0cc
Joanne Lin <y3933y3933@gmail.com>
2026-05-18 00:52:02 +0800

    feat: custom heading id

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,17 @@ import mdx from '@astrojs/mdx';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import remarkCustomHeadingId from 'remark-custom-heading-id';
+
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), mdx()],
 
   vite: {
     plugins: [tailwindcss()]
+  },
+  markdown:{
+    remarkPlugins: [remarkCustomHeadingId]
   }
 });

aa1063f7f0bcce48a289c46dfa0ba4c72882f293
Joanne Lin <y3933y3933@gmail.com>
2026-05-19 00:44:44 +0800

    chore: update site

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://jienianan.com",
   integrations: [react(), mdx(), sitemap()],
 
   vite: {

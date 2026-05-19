5503a3232f03a4266fe8e373dc992ca209f6eb61
Joanne Lin <y3933y3933@gmail.com>
2026-05-19 22:56:56 +0800

    feat: 留言板

---
+++ src/components/GiscusComments.tsx
@@ -0,0 +1,19 @@
+import Giscus from "@giscus/react";
+
+export default function GiscusComments() {
+  return (
+    <Giscus
+      repo="y3933y3933/website"
+      repoId="R_kgDOO4Khdg"
+      category="Announcements"
+      categoryId="DIC_kwDOO4Khds4C9ZJH"
+      mapping="pathname"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="top"
+      theme="preferred_color_scheme"
+      lang="zh-TW"
+      loading="lazy"
+    />
+  );
+}

f1d8e54f307c343c0e024da43179d9f1ce70d3d7
Joanne Lin <y3933y3933@gmail.com>
2026-05-21 01:15:15 +0800

    feat: add memes

--- src/data/memes.ts
+++ src/data/memes.ts
@@ -1,8 +1,8 @@
 export type Meme = {
-  src: string
-  alt: string
-  category: string
-}
+  src: string;
+  alt: string;
+  category: string;
+};
 
 export const memes: Meme[] = [
   // engineer
@@ -10,15 +10,28 @@ export const memes: Meme[] = [
   { src: "/memes/engineer/cartoon.jpg", alt: "cartoon", category: "engineer" },
   { src: "/memes/engineer/push-f.JPG", alt: "push-f", category: "engineer" },
   { src: "/memes/engineer/qa.JPG", alt: "qa", category: "engineer" },
+  {
+    src: "/memes/engineer/communication.PNG",
+    alt: "communication",
+    category: "engineer",
+  },
+
   // job
   { src: "/memes/job/client.JPG", alt: "client", category: "job" },
   { src: "/memes/job/job-pro.PNG", alt: "job-pro", category: "job" },
   { src: "/memes/job/newbie.jpg", alt: "newbie", category: "job" },
   { src: "/memes/job/nomoney.jpg", alt: "no money", category: "job" },
   { src: "/memes/job/ok.jpg", alt: "ok", category: "job" },
+  { src: "/memes/job/six.JPG", alt: "six", category: "job" },
   // product
   { src: "/memes/product/product.JPG", alt: "product", category: "product" },
-  { src: "/memes/product/requirements.JPG", alt: "requirements", category: "product" },
-]
+  {
+    src: "/memes/product/requirements.JPG",
+    alt: "requirements",
+    category: "product",
+  },
+  // other
+  { src: "/memes/other/card.JPG", alt: "card", category: "other" },
+];
 
-export const categories = [...new Set(memes.map((m) => m.category))]
+export const categories = [...new Set(memes.map((m) => m.category))];

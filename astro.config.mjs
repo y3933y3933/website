c6aa18394c0afbcdee29f7bedc9f9e9dcb725cca
Joanne Lin <y3933y3933@gmail.com>
2026-05-21 00:57:24 +0800

    feat: code block

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,41 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import remarkCustomHeadingId from 'remark-custom-heading-id';
+import remarkCustomHeadingId from "remark-custom-heading-id";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
+import expressiveCode from "astro-expressive-code";
 
-import cloudflare from '@astrojs/cloudflare';
-
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jienianan.com",
-adapter: cloudflare({
-  imageService: { build: 'compile', runtime: 'cloudflare-binding' }
-}),  integrations: [react(), mdx(), sitemap()],
+  adapter: cloudflare({
+    imageService: { build: "compile", runtime: "cloudflare-binding" },
+  }),
 
+  integrations: [
+    expressiveCode({
+      themes: ["snazzy-light"],
+      styleOverrides: {
+        borderRadius: "0.5rem",
+      },
+    }),
+    react(),
+    mdx(),
+    sitemap(),
+  ],
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
-  markdown:{
-    remarkPlugins: [remarkCustomHeadingId]
+  markdown: {
+    remarkPlugins: [remarkCustomHeadingId],
   },
-   });
+});

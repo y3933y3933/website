a90af37307d3d9725f7650208119d8fadd295a20
Joanne Lin <y3933y3933@gmail.com>
2026-05-25 17:22:01 +0800

    feat: add isDraft

--- src/content.config.js
+++ src/content.config.js
@@ -1,18 +1,18 @@
-import { defineCollection } from 'astro:content';
-import { glob, file } from 'astro/loaders';
-import { z } from 'astro/zod';
-
+import { defineCollection } from "astro:content";
+import { glob, file } from "astro/loaders";
+import { z } from "astro/zod";
 
 const blog = defineCollection({
-    loader: glob({base :'./src/blog', pattern:'**/*.{md,mdx}'}),
-    schema: ({ image }) => z.object({
-        title: z.string(),
-        description: z.string(),
-        pubDate: z.coerce.date(),
-        tags: z.array(z.string()).default([]),
-        cover: image().optional(),
-    })
-})
+  loader: glob({ base: "./src/blog", pattern: "**/*.{md,mdx}" }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.coerce.date(),
+      tags: z.array(z.string()).default([]),
+      cover: image().optional(),
+      isDraft: z.boolean().default(false),
+    }),
+});
 
-
-export const collections = {blog}
+export const collections = { blog };
